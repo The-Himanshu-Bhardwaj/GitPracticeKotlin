9f4fd8f5dc4937fc7c70c4a5c33d5ad2ed5183d1
himanshu <himanshub1295@gmail.com>
2022-11-22 16:02:42 +0530

    conflict change in exp

--- app/src/main/java/com/example/creative/notes/gitpracticekotlin/MainActivity.kt
+++ app/src/main/java/com/example/creative/notes/gitpracticekotlin/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG, "onCreate: new error")
 
-        Log.d(TAG, "onCreate: third")
+        Log.d(TAG, "onCreate: third---")
         //nfknknnffaknnadfn//
         // kajsdfkhad//nfknknnffaknnadfn//
         //        // kajsdfkhad
